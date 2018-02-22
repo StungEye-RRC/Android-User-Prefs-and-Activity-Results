bc151ddad2538b3a2a839a64ca0e30f03d7766c7
Kyle Geske <stungeye@gmail.com>
2018-02-22 10:32:23 -0600

    Calendar load/save implemented along with returning results using an intent.

--- app/src/main/java/com/stungeye/calculator/CalendarActivity.java
+++ app/src/main/java/com/stungeye/calculator/CalendarActivity.java
@@ -1,13 +1,95 @@
 package com.stungeye.calculator;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    final String preferenceIdentifier = "com.stungeye.calculator";
+    final String PREFS_AGE = "pref_age";
+    final String PREFS_NAME = "pref_name";
+    final int UNSPECIFIED_AGE = -1;
+    public SharedPreferences prefs;
+
+    public EditText ageField;
+    public EditText nameField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        prefs = this.getSharedPreferences(preferenceIdentifier, Context.MODE_PRIVATE);
+
+        ageField = findViewById(R.id.age);
+        nameField = findViewById(R.id.name);
+    }
+
+    /* Make the "up/home" button act like the back button
+       so that we always return our result Intent.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return(super.onOptionsItemSelected(item));
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent data = new Intent();
+        String name = nameField.getText().toString().trim();
+        String ageString  = ageField.getText().toString().trim();
+
+        data.putExtra(MainActivity.EXTRA_NAME, name);
+        data.putExtra(MainActivity.EXTRA_AGE, ageString);
+        setResult(RESULT_OK, data);
+        super.onBackPressed();
+    }
+
+    public void loadPrefs(View view) {
+        String name = prefs.getString(PREFS_NAME, null);
+        int age = prefs.getInt(PREFS_AGE, UNSPECIFIED_AGE);
+
+        String ageString = "";
+
+        if (age != UNSPECIFIED_AGE) {
+            ageString += age;
+        }
+
+        nameField.setText(name);
+        ageField.setText(ageString);
+
+        Toast.makeText(getApplicationContext(), "Name and Age Loaded", Toast.LENGTH_LONG).show();
+    }
+
+    public void savePrefs(View view) {
+        SharedPreferences.Editor editor = prefs.edit();
+        String name = nameField.getText().toString().trim();
+        String ageString  = ageField.getText().toString().trim();
+        int age;
+
+        if (ageString.equals("")) {
+            age = UNSPECIFIED_AGE;
+        } else {
+            age = Integer.valueOf(ageString);
+        }
+
+        editor.putInt(PREFS_AGE, age);
+        editor.putString(PREFS_NAME, name);
+
+        editor.commit();
+
+        Toast.makeText(getApplicationContext(), "Name and Age Saved", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/stungeye/calculator/MainActivity.java
+++ app/src/main/java/com/stungeye/calculator/MainActivity.java
@@ -1,5 +1,7 @@
 package com.stungeye.calculator;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,10 +9,34 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int NAME_AGE_REQUEST = 0;
+    static final String EXTRA_AGE = "extra_age";
+    static final String EXTRA_NAME = "extra_name";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NAME_AGE_REQUEST && resultCode == Activity.RESULT_OK) {
+
+            EditText editName = findViewById(R.id.name);
+            EditText editAge = findViewById(R.id.age);
+
+            editName.setText(data.getStringExtra(EXTRA_NAME));
+            editAge.setText(data.getStringExtra(EXTRA_AGE));
+        }
+    }
+
+    public void startCalendar(View view) {
+        Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+        startActivityForResult(intent, NAME_AGE_REQUEST);
     }
 
     public void calculate(View view) {
